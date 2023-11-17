5a910a4ad9cc00c487331b0fb43fa6a0924b7c62
TLSPC-079 <TLSPC-079@DESKTOP-AVJ9472>
2023-11-17 10:35:40 +0530

    created two new intefaces

---
+++ SOLID_LSP_Demo/IManaged.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_LSP_Demo
+{
+    public interface IManaged : IEmployee
+    {
+        Employee Manager { get; set; }
+        void AssignManager(Employee manager);
+    }
+}
