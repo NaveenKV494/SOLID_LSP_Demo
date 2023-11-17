07244135724a8bac68a74c24ed33e07171169655
TLSPC-079 <TLSPC-079@DESKTOP-AVJ9472>
2023-11-17 10:12:03 +0530

    Created IEmployee interface

--- SOLID_LSP_Demo/Employee.cs
+++ SOLID_LSP_Demo/Employee.cs
@@ -6,25 +6,26 @@ using System.Threading.Tasks;
 
 namespace SOLID_LSP_Demo
 {
-    public class Employee
+    public class Employee : IEmployee
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public Employee Manager { get; set; } = null;
         public decimal Salary { get; set; }
-    }
 
-    public virtual void AssignManager(Employee manager)
-    {
-        // Simulate doing other tasks here - otherwise, this should be
-        // a property set statement, not a method.
-        Manager = manager;
-    }
 
-    public virtual void CalculatePerHourRate(int rank)
-    {
-        decimal baseAmount = 12.50M;
+        public virtual void AssignManager(Employee manager)
+        {
+            // Simulate doing other tasks here - otherwise, this should be
+            // a property set statement, not a method.
+            Manager = manager;
+        }
 
-        Salary = baseAmount + (rank * 2);
+        public virtual void CalculatePerHourRate(int rank)
+        {
+            decimal baseAmount = 12.50M;
+
+            Salary = baseAmount + (rank * 2);
+        }
     }
 }
---
+++ SOLID_LSP_Demo/IEmployee.cs
@@ -0,0 +1,11 @@
+﻿namespace SOLID_LSP_Demo
+{
+    public interface IEmployee
+    {
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        decimal Salary { get; set; }
+
+        void CalculatePerHourRate(int rank);
+    }
+}
